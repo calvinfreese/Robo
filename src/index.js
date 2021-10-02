49e4439f42ff3102001f0c9579df6167b0599bbe
Calvin Freese <calvin.p.freese@gmail.com>
2021-10-01 23:50:18 -0500

    added redux logger and middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import './index.css';
-
-
 import App from './containers/App.js';
 import { searchRobots } from "./redux/reducer"
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
     <Provider store={store}>
