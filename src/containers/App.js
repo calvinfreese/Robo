e73ea5e16c9c59447293d2b63d9948aa83c87e6c
Calvin Freese <calvin.p.freese@gmail.com>
2021-10-01 23:39:42 -0500

    added store and mapped dispatch to props

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from "react-redux";
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from "../components/Scroll";
 import './App.css';
 
+import  { setSearchField } from "../redux/actions.js";
 
-const App = () => {
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch)  => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+const App = (store) => {
 
     const [robots, setRobots] = useState([]);
-    const [searchfield, setSearchfield] = useState('');
+
    
     useEffect( () => {
         fetchRobots();
@@ -20,14 +34,12 @@ const App = () => {
         .then(users => setRobots(users)); 
     }
 
-    const onSearchChange = (event) => {    
-        setSearchfield( event.target.value );  
-   
-    }
+  
 
-   
+    console.log(store);
+    const { searchField, onSearchChange } = store;
     const filteredRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
     return !robots.length ? 
@@ -48,4 +60,4 @@ const App = () => {
     
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import './index.css';
 
+
 import App from './containers/App.js';
+import { searchRobots } from "./redux/reducer"
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
+const store = createStore(searchRobots);
 
-ReactDOM.render(<App /> , document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
